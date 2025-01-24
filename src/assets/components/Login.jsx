b71502fc146e7c9f9520d1dd1ab617d42855e451
juanitomiller <juancristobalcamposferrada@gmail.com>
2025-01-23 23:32:02 -0300

    Upload img Login

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -43,54 +43,56 @@ export const Login = () => {
   };
 
   return (
-    <div className="container mt-5 w-1">
-      <form onSubmit={validarInput}>
-        <fieldset>
-          <legend className="Logintext">Inicio de Sesión</legend>
-          <hr />
-          <div className="mb-3">
-            <label htmlFor="userEmail" className="form-label">
-              Ingrese su correo electrónico
+    <div className="container plox mt-5">
+  <div className="login-form-container">
+    <form onSubmit={validarInput}>
+      <fieldset>
+        <legend className="Logintext">Inicio de Sesión</legend>
+        <hr />
+        <div className="mb-3">
+          <label htmlFor="userEmail" className="form-label">
+            Ingrese su correo electrónico
+          </label>
+          <input
+            type="email"
+            id="userEmail"
+            className="form-control"
+            placeholder="correo@ejemplo.com"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
+        </div>
+        <hr />
+        <div className="row">
+          <div className="mb-3 col">
+            <label htmlFor="userPassword" className="form-label">
+              Ingrese su contraseña
             </label>
             <input
-              type="email"
-              id="userEmail"
+              type="password"
+              id="userPassword"
               className="form-control"
-              placeholder="correo@ejemplo.com"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
+              placeholder="Contraseña"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               required
             />
           </div>
-          <hr />
-          <div className="row">
-            <div className="mb-3 col">
-              <label htmlFor="userPassword" className="form-label">
-                Ingrese su contraseña
-              </label>
-              <input
-                type="password"
-                id="userPassword"
-                className="form-control"
-                placeholder="Contraseña"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                required
-              />
-            </div>
-          </div>
-          <hr />
-          <button type="submit" className="btn btn-dark mt-1">
-            Iniciar sesión
-          </button>
-          <hr />
-        </fieldset>
-      </form>
-      {error && (
-        <p className="bg-danger text-light fs-6 p-3 rounded border border-danger-subtle mt-3">
-          {error}
-        </p>
-      )}
-    </div>
+        </div>
+        <hr />
+        <button type="submit" className="btn btn-dark mt-1">
+          Iniciar sesión
+        </button>
+        <hr />
+      </fieldset>
+    </form>
+    {error && (
+      <p className="bg-danger text-light fs-6 p-3 rounded border border-danger-subtle mt-3">
+        {error}
+      </p>
+    )}
+  </div>
+</div>
   );
 };
